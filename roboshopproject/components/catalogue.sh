ee1824ee6cc20e1a863e740349cafd2419a261ab
My <karumanchi.swapna@gmail.com>
2022-02-18 20:07:24 +0530

    Cantalogue added

--- roboshopproject/components/catalogue.sh
+++ roboshopproject/components/catalogue.sh
@@ -66,8 +66,24 @@ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/cat
   cp -r catalogue-main /home/roboshop/catalogue &>>$Log_file
 
   echo "install NodeJS dependencies"
-  cd /home/roboshop/catalogue
-  npm install &>>$Log_file
+  cd /home/roboshop/catalogue &>>$Log_file
+  npm install &>>
+
+  chown roboshp:roboshop /home/roboshop/ -g
+
+  echo "update systemD file"
+  sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+
+  echo "setop catalogue systemD File"
+  mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$Log_file
+
+  echo "Start Service"
+  systemctl enable catalogue &>>$Log_file
+  systemctl start catalogue &>>$Log_file
+
+
+
+
 
 
 
