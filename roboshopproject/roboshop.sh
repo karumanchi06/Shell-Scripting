c56277cabc19a3a7af1f0d73d7e62fc0cf08bdfa
My <karumanchi.swapna@gmail.com>
2022-02-19 16:38:19 +0530

    Cantalogue added

--- roboshopproject/roboshop.sh
+++ roboshopproject/roboshop.sh
@@ -2,12 +2,12 @@
 
 ID=$(id -u)
 if [ $ID -ne 0 ];then
-  echo -e "\e[1;31m you should be root user to execute this script....\e[om"
+  echo -e "\e[1;31m you should be root user to execute this script....\e[0m"
   exit
   fi
   if  [ -f components/$1.sh ];then
     bash components/$1.sh
 
     else
-      echo -e "\e[1;31minvalid input\e[om"
+      echo -e "\e[1;31minvalid input\e[0m"
       fi
