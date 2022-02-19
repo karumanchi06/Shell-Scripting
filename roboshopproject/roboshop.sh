0dce1db326d6fca90fa578c988742689ccf065a9
My <karumanchi.swapna@gmail.com>
2022-02-19 16:28:44 +0530

    Cantalogue added

--- roboshopproject/roboshop.sh
+++ roboshopproject/roboshop.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-bash components/$1.sh
+ID=$(id -u)
+if [ $ID -ne 0 ];then
+  echo -e "\e [1;31m you should be toot uesr to execute this script....\e[om"
+  exit
+  fi
+  if  [ -f components/$1.sh ];then
+    bash components/$1.sh
+
+    else
+      echo -e "\e [1;31m invalid input\e[om"
+      fi
