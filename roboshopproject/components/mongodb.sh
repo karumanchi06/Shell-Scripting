01932133b400f7273667caef89a98ea0e09b9e35
My <karumanchi.swapna@gmail.com>
2022-02-19 19:19:12 +0530

    Cantalogue added

--- roboshopproject/components/mongodb.sh
+++ roboshopproject/components/mongodb.sh
@@ -48,25 +48,61 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/robos
 
 echo "install mongodb"
 yum install -y  mongodb-org &>>$Log_file
+if [ $? -eq 0 ];then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit
+    fi
 
 echo "update mongodb config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$Log_file
+if [ $? -eq 0 ];then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit
+    fi
 
 echo "start database"
 systemctl enable mongod &>>$Log_file
 systemctl start mongod &>>$Log_file
+if [ $? -eq 0 ];then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit
+    fi
 
 echo "download schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$Log_file
+if [ $? -eq 0 ];then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit
+    fi
 
 echo "extract Schema"
 cd /tmp
 unzip -o mongodb.zip &>>$Log_file
+if [ $? -eq 0 ];then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit
+    fi
 
 echo "load schema"
 cd mongodb-main
 mongo < catalogue.js &>>$Log_file
 mongo < users.js &>>$Log_Log_file
+if [ $? -eq 0 ];then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit
+    fi
 
 
 
