df22436e4c3348b2ba09c51984b9ac472a67c68b
My <karumanchi.swapna@gmail.com>
2022-02-19 19:37:44 +0530

    Cantalogue added

--- roboshopproject/components/redis.sh
+++ roboshopproject/components/redis.sh
@@ -20,9 +20,9 @@ if [ $? -eq 0 ];then
 
     echo "update redis configuration"
     if [ -f /etc/redis.conf ]; then
-      sed -i -e "s\127.0.0.1/0.0.0.0/g" /etc/redis.conf &>>$Log_file
+      sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>$Log_file
       elif [ /etc/redis/redis.conf ]; then
-        sed -i -e "s\127.0.0.1/0.0.0.0/g" /etc/redis.conf &>>$Log_file
+        sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>>$Log_file
         fi
         if [ $? -eq 0 ];then
           echo -e "\e[1;32m SUCCESS\e[0m"
