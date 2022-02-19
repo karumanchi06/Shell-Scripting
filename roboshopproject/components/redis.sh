2a54f9476eb5476080757fbeccc234ddf48c958d
My <karumanchi.swapna@gmail.com>
2022-02-19 19:16:00 +0530

    Cantalogue added

--- roboshopproject/components/redis.sh
+++ roboshopproject/components/redis.sh
@@ -19,5 +19,23 @@ source components/common.sh
 
 echo "configure redis repo"
 curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>$Log_file
+if [ $? -eq 0 ];then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit
+    fi
+
+echo "install redis"
+yum install redis -y &>>$Log_file
+if [ $? -eq 0 ];then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+    exit
+    fi
+
+
+
 
 
